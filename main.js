ce4351c88c6aac65bedd133606bd32d3b9ebe451
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-04-11 22:00:11 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,115 +1,107 @@
-const timeRef = document.getElementById("time");
-const greetingRef = document.getElementById("greeting");
-const nameRef = document.getElementById("name");
-const focusRef = document.getElementById("focus");
+// DOM Elements
+const time = document.getElementById('time'),
+  greeting = document.getElementById('greeting'),
+  name = document.getElementById('name'),
+  focus = document.getElementById('focus');
 
 // Options
 const showAmPm = true;
 
 // Show Time
 function showTime() {
-  //   let today = new Date(2021, 06, 10, 20, 33, 30),
   let today = new Date(),
     hour = today.getHours(),
-    minutes = today.getMinutes(),
-    seconds = today.getSeconds();
+    min = today.getMinutes(),
+    sec = today.getSeconds();
 
   // Set AM or PM
-  const amPm = hour >= 12 ? "PM" : "AM";
+  const amPm = hour >= 12 ? 'PM' : 'AM';
 
   // 12hr Format
-  // hour = hour % 12 || 12;
+  hour = hour % 12 || 12;
 
   // Output Time
-  timeRef.innerHTML = `${hour}<span>:</span>${addZero(
-    minutes
-  )}<span>:</span>${addZero(seconds)} ${showAmPm ? amPm : ""}`;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )} ${showAmPm ? amPm : ''}`;
 
   setTimeout(showTime, 1000);
 }
 
 // Add Zeros
 function addZero(n) {
-  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+  return (parseInt(n, 10) < 10 ? '0' : '') + n;
 }
 
 // Set Background and Greeting
 function setBgGreet() {
-  // let today = new Date(2021, 06, 10, 20, 33, 30),
   let today = new Date(),
     hour = today.getHours();
-  document.body.style.color = "white";
-  if (hour < 3) {
-    // Night
-    document.body.style.backgroundImage = "url('./images/night.jpg')";
-    greetingRef.textContent = "Good Night, ";
-  } else if (hour < 12) {
+
+  if (hour < 12) {
     // Morning
-    document.body.style.backgroundImage = "url('./images/morning.jpg')";
-    greetingRef.textContent = "Good Morning, ";
+    document.body.style.backgroundImage = "url('f59b27f7-f566-4fd5-ba58-2e80e2b7e855.png')";
+    greeting.textContent = 'Good Morning, ';
   } else if (hour < 18) {
     // Afternoon
-    document.body.style.backgroundImage = "url('./images/afternoon.jpg')";
-    greetingRef.textContent = "Good Afternoon, ";
-  } else if (hour < 22) {
-    // Evening
-    document.body.style.backgroundImage = "url('./images/evening.jpg')";
-    greetingRef.textContent = "Good Evening, ";
+    document.body.style.backgroundImage = "url('beautiful-view-mesmerizing-nature-traditional-styled-japanese-adelaide-himej')";
+    greeting.textContent = 'Good Afternoon, ';
   } else {
-    // Night
-    document.body.style.backgroundImage = "url('./images/night.jpg')";
-    greetingRef.textContent = "Good Night, ";
+    // Evening
+    document.body.style.backgroundImage = "url('a01b616e-e1ce-40b8-8dd2-9d144e8db06f.png' )";
+    greeting.textContent = 'Good Evening, ';
+    document.body.style.color = 'white';
   }
 }
 
 // Get Name
 function getName() {
-  if (localStorage.getItem("name") === null) {
-    nameRef.textContent = "[Enter Name]";
+  if (localStorage.getItem('name') === null) {
+    name.textContent ='Enter Name';
   } else {
-    nameRef.textContent = localStorage.getItem("name");
+    name.textContent = localStorage.getItem('name');
   }
 }
 
 // Set Name
 function setName(e) {
-  if (e.type === "keypress") {
+  if (e.type === 'keypress') {
     // Make sure enter is pressed
     if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem("name", e.target.innerText);
-      nameRef.blur();
+      localStorage.setItem('name', e.target.innerText);
+      name.blur();
     }
   } else {
-    localStorage.setItem("name", e.target.innerText);
+    localStorage.setItem('name', e.target.innerText);
   }
 }
 
 // Get Focus
 function getFocus() {
-  if (localStorage.getItem("focus") === null) {
-    focusRef.textContent = "[Enter Focus]";
+  if (localStorage.getItem('focus') === null) {
+    focus.textContent = 'Enter Focus';
   } else {
-    focusRef.textContent = localStorage.getItem("focus");
+    focus.textContent = localStorage.getItem('focus');
   }
 }
 
 // Set Focus
 function setFocus(e) {
-  if (e.type === "keypress") {
+  if (e.type === 'keypress') {
     // Make sure enter is pressed
     if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem("focus", e.target.innerText);
-      focusRef.blur();
+      localStorage.setItem('focus', e.target.innerText);
+      focus.blur();
     }
   } else {
-    localStorage.setItem("focus", e.target.innerText);
+    localStorage.setItem('focus', e.target.innerText);
   }
 }
 
-nameRef.addEventListener("keypress", setName);
-nameRef.addEventListener("blur", setName);
-focusRef.addEventListener("keypress", setFocus);
-focusRef.addEventListener("blur", setFocus);
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 
 // Run
 showTime();
