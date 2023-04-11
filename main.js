8fc46f9bb9b3cab63f43b94666bb9a8014d91ce6
sultanarebaka <117830026+sultanarebaka@users.noreply.github.com>
2023-04-11 22:19:30 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function setBgGreet() {
 // Get Name
 function getName() {
   if (localStorage.getItem('name') === null) {
-    name.textContent ='Enter Name';
+    name.textContent ='Somapti';
   } else {
     name.textContent = localStorage.getItem('name');
   }
@@ -79,7 +79,7 @@ function setName(e) {
 // Get Focus
 function getFocus() {
   if (localStorage.getItem('focus') === null) {
-    focus.textContent = 'Enter Focus';
+    focus.textContent = 'Make Videos';
   } else {
     focus.textContent = localStorage.getItem('focus');
   }
